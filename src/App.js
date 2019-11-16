fdfe06e9ab576a38b71884e129bcb6e110090ba2
DeAndre Johnson <dre@phreshr.com>
2019-11-16 14:45:06 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './routes/Home';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div className="App">
+        <Switch>
+          <Route exact path="/login">
+            {localStorage.getItem('token') ? <Redirect to="/" /> : <Login />}
+          </Route>
+          <PrivateRoute path="/">
+            <Home />
+          </PrivateRoute>
+        </Switch>
+      </div>
+    </>
+  );
+}
+
+export default App;
