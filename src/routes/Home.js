c0be12e4238321bd355823ba53f49edbf9874e81
DeAndre Johnson <dre@phreshr.com>
2019-11-17 11:37:36 +0800

    add a logout button

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useAuth } from '../hooks/useAuth';
 
 const Home = () => {
+  const { handleLogout } = useAuth();
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={handleLogout}>Log out</button>
     </div>
   );
 };
