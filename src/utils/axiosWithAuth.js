31c1f5d72ce7e9c91b52dc4b173fc4b4e15bc621
DeAndre Johnson <dre@phreshr.com>
2019-11-20 00:57:29 +0800

    removed bearer from authorization

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -6,7 +6,7 @@ const axiosWithAuth = () => {
     baseURL: 'https://patrick-diy.herokuapp.com',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`, // 9d8a1097-5d0f-4960-87e3-88c251582f1d,
+      Authorization: token,
     },
   });
 };
