280e91c18f9916352992a1f1eecf8f1e0812d650
Courtland Bourgeois <courtland@CourtlandsMBP2.eatel.net>
2019-11-19 09:43:23 -0600

    adding registration form and testing login endpoint

---
+++ src/components/RegisterForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const RegisterForm = ({ values, errors, touched, status }) => {
+    const [userSignUp, setUserSignUp] = useState([])
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RegisterForm
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const axiosWithAuth = () => {
   let token = localStorage.getItem('token');
   return axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://patrick-diy.herokuapp.com',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + token,
+      Authorization: `Bearer ${token}`,
     },
   });
 };
