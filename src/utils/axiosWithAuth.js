53324dc4d6577d585e675f22a3b63db70251ff5b
DeAndre Johnson <dre@phreshr.com>
2019-11-20 02:51:58 +0800

    update axios helper

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,13 +2,15 @@ import axios from 'axios';
 
 const axiosWithAuth = () => {
   let token = localStorage.getItem('token');
-  return axios.create({
+  let instance = axios.create({
     baseURL: 'https://patrick-diy.herokuapp.com',
     headers: {
-      'Content-Type': 'application/json',
-      Authorization: token,
+      'Content-Type': 'application/json;charset=UTF-8',
+      Authorization: 'Bearer ' + JSON.parse(token),
     },
   });
+
+  return instance;
 };
 
 export default axiosWithAuth;
