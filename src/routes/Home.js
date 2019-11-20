98813ac2458d659cb0048b4bec69c783cbc8cbdf
DeAndre Johnson <dre@phreshr.com>
2019-11-21 01:06:29 +0800

    display current username

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/macro';
 import { CSSTransition } from 'react-transition-group';
-import { Plus, Minus } from 'react-feather';
+import { PlusCircle, Plus, Minus } from 'react-feather';
 import axiosWithAuth from '../utils/axiosWithAuth';
 import { useAuth } from '../hooks/useAuth';
 
@@ -10,6 +10,12 @@ import AddForm from '../components/AddForm';
 
 const Wrapper = styled.div`
   padding: 2rem;
+
+  h2 {
+    span {
+      color: indianred;
+    }
+  }
 `;
 
 const UserSection = styled.div`
@@ -30,10 +36,17 @@ const UserToolItem = styled.div`
   border-radius: 15px;
   padding: 1rem;
   scroll-snap-align: start;
+
+  div.add-project {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const Home = () => {
   const [projects, setProjects] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
   const [showAddForm, setShowAddForm] = useState(false);
   const { handleLogout } = useAuth();
 
@@ -41,26 +54,37 @@ const Home = () => {
 
   useEffect(() => {
     (async () => {
+      const userRes = await axiosWithAuth().get('users/getuserinfo');
+      const user = await userRes.data;
+      setCurrentUser(user);
+    })();
+    console.log(currentUser);
+  }, []);
+
+  useEffect(() => {
+    (async () => {
       const res = await axiosWithAuth().get('/projects/projects');
       const data = await res.data;
       setProjects(data);
     })();
-  }, []);
+  }, [setProjects]);
   return (
     <Wrapper>
-      <h1>Discover</h1>
-      <button onClick={handleLogout}>Log out</button>
-
+      <h2>
+        hello <span>{currentUser.username}</span>
+      </h2>
       <CSSTransition in={showAddForm} timeout={300} classNames="show-form">
         <AddForm open={showAddForm} close={toggleAddForm} />
       </CSSTransition>
       <UserSection>
         <UserToolItem>
           <div className="add-project" onClick={toggleAddForm}>
-            {showAddForm ? <Minus size="48" /> : <Plus size="48" />}
+            <PlusCircle size="48" />
+            <h3>Add New</h3>
           </div>
         </UserToolItem>
       </UserSection>
+      <h2>Discover</h2>
       <ProjectsFeed projects={projects} setProjects={setProjects} />
     </Wrapper>
   );
